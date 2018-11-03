ac2b772e45da4a7f745356790cb8928ed4343e35
Lars <lars.berger@yahoo.com>
2018-11-03 19:52:17 +0100

    feat: layout complete

--- src/containers/Explore/Explore.vue
+++ src/containers/Explore/Explore.vue
@@ -1,75 +1,80 @@
 <template>
   <div class="explore">
-    <h1>This is the explore page</h1>
+    <nav class="sidebar">
 
-    <GmapMap
-      v-if="getLongitude !== 0"
-      :center="{ lat: latitude, lng: longitude }"
-      :zoom="15"
-      style="width: 100%; height: 500px"
-    >
-      <GmapMarker
-        v-for="(item, index) in listings"
-        :key="index"
-        :position="{lat: Number(item.latitude), lng: Number(item.longitude)}"
-        :clickable="true"
-        @click="openModal(item)"
-      />
-  </GmapMap>
+    </nav>
+    <main>
+      <aside class="top">
+        <h1>Lending items</h1>
+        <input type="text" name="" id="">
+        <select name="" id="" class="top-langSelect"></select>
+      </aside>
+      
+      <div class="content">
+        <div class="list">
+          <!-- <p>fdijsoaof</p> -->
+        </div>
+        <div class="map">
+            <!-- <p>fdijsfijijijoaof</p> -->
+  
+        </div>
+      </div>
+      <h1>This is the explore page</h1>
 
-    <MapsMarkerModal v-if="modal.visible" :details="modal.selectedListing" :closeModal="closeModal" />
+    </main>
+
   </div>
 </template>
 
 <script>
-import listings from "./listings.json";
-import MapsMarkerModal from "@/components/MapsMarkerModal/MapsMarkerModal.vue";
+  import listings from "./listings.json";
+  import MapsMarkerModal from "@/components/MapsMarkerModal/MapsMarkerModal.vue";
 
-export default {
-  name: "Explore",
-  props: {
-    msg: String
-  },
-  components: {
-    MapsMarkerModal
-  },
-  data: function() {
-    return {
-      latitude: "",
-      longitude: "",
-      listings: [],
-      modal: {
-        visible: false,
-        selectedListing: {}
-      }
-    };
-  },
-  methods: {
-    openModal: function(item) {
-      this.modal.selectedListing = item;
-      this.modal.visible = true;
+  export default {
+    name: "Explore",
+    props: {
+      msg: String
     },
-    closeModal: function() {
-      this.modal.visible = false;
-    }
-  },
-  mounted: function() {
-    const getLocation = () => {
-      navigator.geolocation.getCurrentPosition(position => {
-        this.latitude = position.coords.latitude;
-        this.longitude = position.coords.longitude;
-      });
-    };
-    getLocation();
+    components: {
+      MapsMarkerModal
+    },
+    data: function () {
+      return {
+        latitude: "",
+        longitude: "",
+        listings: [],
+        modal: {
+          visible: false,
+          selectedListing: {}
+        }
+      };
+    },
+    methods: {
+      openModal: function (item) {
+        this.modal.selectedListing = item;
+        this.modal.visible = true;
+      },
+      closeModal: function () {
+        this.modal.visible = false;
+      }
+    },
+    mounted: function () {
+      const getLocation = () => {
+        navigator.geolocation.getCurrentPosition(position => {
+          this.latitude = position.coords.latitude;
+          this.longitude = position.coords.longitude;
+        });
+      };
+      getLocation();
 
-    const getListings = () => {
-      this.listings = listings;
-    };
-    getListings();
-  }
-};
+      const getListings = () => {
+        this.listings = listings;
+      };
+      getListings();
+    }
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped src="./Explore.css">
 </style>
